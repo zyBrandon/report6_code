0108c3a9ae5e733919bfbeea571b11f4a9f712c1
18779106195 <>
2018-12-18 11:00:21 +0800

    first

--- Layout_code/ViewController.swift
+++ Layout_code/ViewController.swift
@@ -13,6 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let redView = UIView()
+        redView.backgroundColor = UIColor.red
+        let greenView = UIView()
+        greenView.backgroundColor = UIColor.green
+        let yellowView = UIView()
+        yellowView.backgroundColor = UIColor.yellow
+        
+        let stackView = UIStackView(arrangedSubviews: [redView,greenView,yellowView])
+        
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
